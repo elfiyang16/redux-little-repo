d1ed807656f90645318f10b2c3807aad8b8ad698
Elfi Yang <elfiyang16@gmail.com>
2020-06-22 21:42:55 +0100

    feat: redux basic and async call

---
+++ index.old.js
@@ -0,0 +1,85 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
+console.log("from index.js");
+
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "first redux action",
+  };
+}
+
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "first redux action",
+  };
+}
+
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20,
+// };
+
+const initialCake = {
+  numOfCakes: 10,
+};
+
+const initialIceCream = {
+  numOfIceCreams: 20,
+};
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return { ...state, numOfCakes: state.numOfCakes - 1 };
+//     case BUY_ICECREAM:
+//       return { ...state, numOfIceCreams: state.numOfIceCreams - 1 };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCake, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return { ...state, numOfCakes: state.numOfCakes - 1 };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCream, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return { ...state, numOfIceCreams: state.numOfIceCreams - 1 };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer);
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("update state", store.getState());
+});
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+
+unsubscribe();
