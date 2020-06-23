21608b47d6257bd8a517a3fcd5db22c8c1500598
Elfi Yang <elfiyang16@gmail.com>
2020-06-23 09:47:48 +0100

    feat: map state to props advanced

--- react-redux/react-redux/src/App.js
+++ react-redux/react-redux/src/App.js
@@ -6,15 +6,19 @@ import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <CakeContainer />
         <HooksCakeContainer />
         <NewCakeContainer />
         <IceCreamContainer />
+        <ItemContainer cake />
+        <ItemContainer />
       </div>
     </Provider>
   );
